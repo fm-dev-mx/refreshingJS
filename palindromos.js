7809998ad3a2b46f99bb9fa9d6e9b1c3a8f47df9
Francisco Mendoza <frgmendoza@gmail.com>
2022-04-18 05:37:57 -0600

    refreshing JS

---
+++ palindromos.js
@@ -0,0 +1,23 @@
+﻿function solution(inputString) {
+    charNUmbers = inputString.length;
+
+    var isNone = (charNUmbers % 2 == 0) ? midChar = ~~(charNUmbers / 2) : midChar = (~~charNUmbers / 2) + 1;
+
+    console.log("la palabra " + inputString + " tiene " + charNUmbers + " letras");
+    console.log("la mitad de esta palabra es en la letra número " + midChar + " = "+ inputString.charAt[midChar]);
+
+    var ban;
+
+    for(i=0;i <= inputString; i++)
+    {
+        if (inputString.charAt[i]==inputString.charAt[charNUmbers-i])
+            ban = true;
+        else
+            ban = false;
+
+        console.log (inputString[i] + "[" + i + "] vs " + inputString[charNUmbers-i] + "[" + (charNUmbers-i) + "] = " +ban);
+    }
+
+    return ban;
+
+}
